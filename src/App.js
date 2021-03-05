ea6bd6b57305d63324e7faf222730414e1db2dd8
anclaev <iahugo@yandex.ru>
2021-03-05 18:35:38 +0400

    create: 'messages' component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.scss";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Friends from "./components/Friends";
-import Profile from "./components/Profile";
+//import Profile from "./components/Profile";
+import Messages from "./components/Messages";
 
 const App = () => (
   <div className="app-wrapper">
     <Header title="Old Boy" />
     <Menu items={["Me", "Messages", "News", "Music"]} />
     <Friends className="friends" humans={["Andrew", "Anna", "Oleg", "Sasha"]} />
-    <Profile />
+    {/* <Profile /> */}
+    <Messages />
   </div>
 );
 
---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,9 @@
+import s from "./messages.module.sass";
+
+const Messages = () => (
+  <div className={s.messages}>
+    <span className={s.title}>Your messages</span>
+  </div>
+);
+
+export default Messages;
