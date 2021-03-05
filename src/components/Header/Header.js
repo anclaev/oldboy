31a7dd5b3b626f00d693a9766fd9b00c95867a55
anclaev <iahugo@yandex.ru>
2021-03-05 12:24:52 +0400

    fix: header => css-module

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,14 @@
-import "./header.sass";
+import s from "./header.module.sass";
 
 import Icon from "../Icons";
 
 const Header = (props) => (
-  <header className="header">
-    <div className="header-logo">
-      <Icon name="Logo" className="header-logo__icon" />
-      <span className="header-logo__text">{props.title}</span>
+  <header className={s.header}>
+    <div className={s.logo}>
+      <Icon name="Logo" className={s.icon} />
+      <span className={s.text}>{props.title}</span>
     </div>
-    <Icon
-      name="Settings"
-      color="#A7A7A7"
-      size="20"
-      className="header__settings"
-    />
+    <Icon name="Settings" color="#A7A7A7" size="20" className={s.settings} />
   </header>
 );
 
