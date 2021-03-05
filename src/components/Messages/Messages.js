2c6ad9f7c4e5f1ad8fc0f42936fdd122ecd961c1
anclaev <iahugo@yandex.ru>
2021-03-05 18:38:26 +0400

    fix: 'messages' component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,11 @@
 import s from "./messages.module.sass";
 
+import Chat from "../Chat";
+
 const Messages = () => (
   <div className={s.messages}>
     <span className={s.title}>Your messages</span>
+    <Chat />
   </div>
 );
 
